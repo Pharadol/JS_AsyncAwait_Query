d839e3bbf1c84640a12d00a6c0f45d3740a87b3e
Pharadol <pharadol.n@gmail.com>
2023-09-11 14:07:02 +0700

    learn data set

---
+++ 6_data-set.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let dataSet = document.getElementById("data-set");
+  console.log(dataSet.dataset);
+
+//   let exLink = document.getElementById("example-link");
+//   let wikiLinkj = document.getElementById("wiki-link");
+  let iframe = document.getElementById("iframe-area");
+
+//   exLink.addEventListener("click", (event) => {
+//     callIframe(event.target.dataset.href);
+//     console.log(event.target.dataset.href);
+//   });
+
+//   wikiLinkj.addEventListener("click", (event) => {
+//     callIframe(event.target.dataset.href);
+//     console.log(event.target.dataset.href);
+//   });
+
+  function callIframe(link) {
+    iframe.setAttribute("src", link);
+  }
+
+  let a = document.querySelectorAll("a[data-href]");
+
+  for (let el of a) {
+    el.addEventListener("click", (event) => {
+      callIframe(event.target.dataset.href);
+      console.log(event.target.dataset.href);
+    });
+  }
+});
